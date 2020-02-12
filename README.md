6f203174be392060ee12611a4b1de717baeb00ac
selfup <boudinot.regis@yahoo.com>
2020-02-12 16:13:17 -0600

    install go on linux

---
+++ README.md
@@ -0,0 +1,25 @@
+# Linux Go
+
+Install go on linux!
+
+### Steps and Warnings
+
+:warning: Directories will be wiped :warning:
+
+This is how you install new versions of go. Everything is automated :tada:
+
+`$HOME/go` AND/OR `/usr/local/go` drectories will be blown away.
+
+`sudo` will be required only if your go install is already in: `/usr/local/go`
+
+go will be installed in your homedir as so: `$HOME/go`
+
+`GOROOT` will be: `$HOME/go`
+
+`GOPATH` will be: `$HOME/golang`
+
+This install script if intended for non root users.
+
+This will not be good production/cronjob/scheduled job machines that typically run as root.
+
+This script installs go in your user home dir and does not require sudo.
---
+++ main.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Please Read the LICENSE and README in the repo: https://github.com/selfup/linux_go.git
+# If you are reading this because you forked or cloned the repo you know where to find the files
+
+set -e
+
+if [[ -d $HOME/go ]]
+then
+    rm -rf $HOME/go
+fi
+
+if [[ -d /usr/local/go ]]
+then
+    rm -rf /usr/local/go
+fi
+
+GO_DL_VERSION=go1.13.7
+
+wget https://dl.google.com/go/$GO_DL_VERSION.linux-amd64.tar.gz
+
+tar -C $HOME -xzf $GO_DL_VERSION.linux-amd64.tar.gz
+
+mkdir -p $HOME/golang/src/github.com
+mkdir -p $HOME/golang/src/gitlab.com
+mkdir -p $HOME/golang/src/bitbucket.org
+
+touch $HOME/.bashrc
+
+GO_ROOT_SET=$(cat $HOME/.bashrc | grep -q 'GOROOT=$HOME/go' || 1)
+GO_PATH_SET=$(cat $HOME/.bashrc | grep -q 'GOPATH=$HOME/golang' || 1)
+PATH_GO_SET=$(cat $HOME/.bashrc | grep -q 'PATH=$PATH:$GOROOT/bin:$GOPATH/bin' || 1)
+
+if [[ $GO_ROOT_SET -eq 1 ]]
+then
+    echo 'export GOROOT=$HOME/go' >> $HOME/.bashrc
+fi
+
+if [[ $GO_PATH_SET -eq 1 ]]
+then
+    echo 'export GOPATH=$HOME/golang' >> ~/.bashrc
+fi
+
+if [[ $PATH_GO_SET -eq 1 ]]
+then
+    echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> ~/.bashrc
+fi
+
+source $HOME/.bashrc
+
+go version
+
+rm $GO_DL_VERSION.linux-amd64.tar.gz
