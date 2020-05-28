3eef9c41e7b2a4feffcb55a017c4075736963856
Regis <boudinot.regis@yahoo.com>
2020-05-27 19:49:47 -0500

    add golang install for mac

---
+++ mac/golang.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Please Read the LICENSE and README in the repo: https://github.com/selfup/linux_go.git
+# If you are reading this because you forked or cloned the repo you know where to find the files
+
+set -e
+
+if [[ -d $HOME/go ]]
+then
+    rm -rf $HOME/go
+fi
+
+if [[ -d /usr/local/go ]]
+then
+    echo 'password might be required because go was found in /usr/local/go'
+    
+    sleep 1s
+    
+    sudo rm -rf /usr/local/go
+fi
+
+GO_DL_VERSION=go1.14.3
+
+if [[ ! -f $GO_DL_VERSION.darwin-amd64.tar.gz ]]
+then
+    wget https://dl.google.com/go/$GO_DL_VERSION.darwin-amd64.tar.gz
+fi
+
+tar -C $HOME -xzf $GO_DL_VERSION.darwin-amd64.tar.gz
+
+mkdir -p $HOME/golang/src/github.com
+mkdir -p $HOME/golang/src/gitlab.com
+mkdir -p $HOME/golang/src/bitbucket.org
+
+touch $HOME/.bash_profile
+
+GO_ROOT_SET=$(cat $HOME/.bash_profile | grep -q 'GOROOT=$HOME/go' || echo '9042')
+GO_PATH_SET=$(cat $HOME/.bash_profile | grep -q 'GOPATH=$HOME/golang' || echo '9042')
+PATH_GO_SET=$(cat $HOME/.bash_profile | grep -q 'PATH=$PATH:$GOROOT/bin:$GOPATH/bin' || echo '9042')
+
+if [[ $GO_ROOT_SET -eq '9042' ]]
+then
+    echo 'export GOROOT=$HOME/go' >> $HOME/.bash_profile
+fi
+
+if [[ $GO_PATH_SET -eq '9042' ]]
+then
+    echo 'export GOPATH=$HOME/golang' >> ~/.bash_profile
+fi
+
+if [[ $PATH_GO_SET -eq '9042' ]]
+then
+    echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> ~/.bash_profile
+fi
+
+source $HOME/.bash_profile
+
+rm $GO_DL_VERSION.darwin-amd64.tar.gz
+
+go version
