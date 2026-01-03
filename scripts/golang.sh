3edfc0c9222a97d0a51d424ae4ce40f2da4fb943
selfup <selfup@users.noreply.github.com>
2026-01-03 13:25:42 -0600

    add instructions to script output - formatting

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -67,28 +67,30 @@ else
     os_not_supported
 fi
 
-echo "--- shell config file is: $shell_config ---"
+echo "--- shell config file is: $shell_config"
 sleep 1
-echo "--- OS is: $os_type ---"
+echo "--- OS is: $os_type"
 sleep 1
 
 if [[ -d $HOME/go ]]
 then
     gv=$(go version)
 
-    echo "--- current installation: $gv ---"
+    echo "--- current installation: $gv"
 
     if [[ $(echo $gv | grep $GO_DL_VERSION) != "" ]]
     then
         if [[ $overwrite -eq "0" ]]
         then
-            echo "--- current go version ${GO_DL_VERSION} already installed and overwrite flag not provided ---"
-            echo "--- exiting ---"
+            echo "--- current go version ${GO_DL_VERSION} already installed and overwrite flag not provided"
+            echo "--- please add --force to force an overwrite"
+            echo "--- example: ./scripts/golang.sh --force"
+            echo "--- exiting"
             exit 0
         fi
     fi
 
-    echo '--- uninstalling previous go version ---'
+    echo '--- uninstalling previous go version'
     rm -rf $HOME/go || (
         echo "need sudo - please delete $HOME/go with sudo before running script" &&
         echo "PLEASE SAVE YOUR WORK" &&
@@ -120,48 +122,48 @@ fi
 
 if [[ ! -f $go_tarball ]]
 then
-    echo '--- downloading new go version ---'
+    echo '--- downloading new go version'
     curl https://dl.google.com/go/$go_tarball > $go_tarball
 fi
 
-echo '--- unpacking tarball ---'
+echo '--- unpacking tarball'
 tar -C $HOME -xzf $go_tarball
 
-echo '--- checking and creating common paths in GOTPATH ---'
+echo '--- checking and creating common paths in GOTPATH'
 mkdir -p $HOME/golang/src/github.com
 mkdir -p $HOME/golang/src/gitlab.com
 mkdir -p $HOME/golang/src/bitbucket.org
 
-echo '--- updating shell metadata ---'
+echo '--- updating shell metadata'
 touch $HOME/$shell_config
 
-echo '--- checking for GOROOT - GOPATH - PATH ---'
+echo '--- checking for GOROOT - GOPATH - PATH'
 GO_ROOT_SET=$(cat $HOME/$shell_config | grep -q 'GOROOT=$HOME/go' || echo '404')
 GO_PATH_SET=$(cat $HOME/$shell_config | grep -q 'GOPATH=$HOME/golang' || echo '404')
 PATH_GO_SET=$(cat $HOME/$shell_config | grep -q 'PATH=$PATH:$GOROOT/bin:$GOPATH/bin' || echo '404')
 
 if [[ $GO_ROOT_SET -eq '404' ]]
 then
-    echo '--- setting GOROOT ---'
+    echo '--- setting GOROOT'
     echo 'export GOROOT=$HOME/go' >> $HOME/$shell_config
 fi
 
 if [[ $GO_PATH_SET -eq '404' ]]
 then
-    echo '--- setting GOPATH ---'
+    echo '--- setting GOPATH'
     echo 'export GOPATH=$HOME/golang' >> $HOME/$shell_config
 fi
 
 if [[ $PATH_GO_SET -eq '404' ]]
 then
-    echo '--- updating PATH ---'
+    echo '--- updating PATH'
     echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> $HOME/$shell_config
 fi
 
-echo "--- sourcing $shell_config ---"
+echo "--- sourcing $shell_config"
 source $HOME/$shell_config \
-    && echo '--- removing tarball ---' \
+    && echo '--- removing tarball' \
     && rm $go_tarball \
-    && echo '--- checking go version ---' \
+    && echo '--- checking go version' \
     && go version \
     && exit 1 | $SHELL
